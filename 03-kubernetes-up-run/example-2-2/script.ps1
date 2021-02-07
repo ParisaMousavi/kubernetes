f04fca226f7c22b731cfe852eaf044e32df6f4ac
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2021-02-07 12:43:55 +0100

    add example-2-3

---
+++ 03-kubernetes-up-run/example-2-2/script.ps1
@@ -0,0 +1,2 @@
+# change to 03-kuber.... folder and run this command
+docker build -t example-2-2 -f .\example-2-2\dockerfile .\kuard\
---
+++ 03-kubernetes-up-run/example-2-3/script.ps1
@@ -0,0 +1,2 @@
+# change to 03-kuber.... folder and run this command
+docker build -t example-2-3 -f .\example-2-3\dockerfile .\kuard\
