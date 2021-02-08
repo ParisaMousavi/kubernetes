3c87da37de107bb27a1e771d5fcfe91f8504bb25
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2021-02-08 17:46:54 +0100

    Create new context for kubectl

---
+++ 03-kubernetes-up-run/example-4-1-new-context/script.ps1
@@ -0,0 +1,12 @@
+# the kubectl's config is located at $HOME/.kube/config.
+
+# create new context
+kubectl config set-context helloworld-context --namespace=helloworld --users ... --clusters ....
+
+# context can be configured for
+# - a specific namespace
+# - a specific user for authentication
+# - a specific cluster, where the specific user can authenticate
+
+# using the newly created context
+kubectl config use-context helloworld-context
